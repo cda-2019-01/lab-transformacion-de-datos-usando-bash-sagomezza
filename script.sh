03a0c80b39249cf75fe8fdb173b97c2ab27780b0
Santaigo Gómez <sagomezza@unal.edu.co>
2019-05-21 17:34:28 -0500

    Final commit

--- script.sh
+++ script.sh
@@ -1,28 +1,13 @@
-# Escriba su código aquí
-
-# for file in data*.csv
-# do
-#     while read line; 
-#     do
-#         for section in $line
-#         do
-#             for character in $section
-#             do  
-#                 echo $file,$character
-#             done
-#         done
-#     done < $file
-# done
-
-x=1
 a=[]
 i=0
 mkdir output-data
-sed "4 d" input-data/data1.csv > output-data/data1.csv
-sed "1 d" input-data/data2.csv > output-data/data2.csv
-cp input-data/data3.csv ./output-data/data3.csv
+sed "4 d" data1.csv > output-data/data1.csv
+sed "1 d" data2.csv > output-data/data2.csv
+cp data3.csv ./output-data/data3.csv
+echo "" >> ./output-data/data3.csv
 for file in output-data/data*.csv
 do
+    x=1
     while read line; 
     do
         for section in $line 
@@ -33,10 +18,11 @@ do
         echo ${a[1]} > output-data/aux.csv
         for res in $(sed 's/,/ /g' output-data/aux.csv) 
         do  
-            echo $file,$x,${a[0]},$res
+            echo ${file##*/},$x,${a[0]},$res
         done
         x=$((x+1))
-        x=1
         i=0
     done < $file
 done
+
+rm -rf output-data
