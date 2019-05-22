33cea59a575c82fb95b7173a33ed64b6265b36e7
Santaigo Gómez <sagomezza@unal.edu.co>
2019-05-22 18:25:14 -0500

    Better solution
    This is a better solution, the previous one was pushed in order to have a record of it.

--- script.sh
+++ script.sh
@@ -1,28 +1,9 @@
-a=[]
-i=0
 mkdir output-data
 sed "4 d" data1.csv > output-data/data1.csv
 sed "1 d" data2.csv > output-data/data2.csv
 cp data3.csv ./output-data/data3.csv
-echo "" >> ./output-data/data3.csv
 for file in output-data/data*.csv
 do
-    x=1
-    while read line; 
-    do
-        for section in $line 
-        do
-            a[$i]=$section
-            i=$((i+1))
-        done
-        echo ${a[1]} > output-data/aux.csv
-        for res in $(sed 's/,/ /g' output-data/aux.csv) 
-        do  
-            echo ${file##*/},$x,${a[0]},$res
-        done
-        x=$((x+1))
-        i=0
-    done < $file
+    awk -v x=$file '{print x ";" FNR ";" $0}' $file | sed 's#output-data/data#data#g' | sed 's#[[:space:]]#|#g' | sed 's/|\+/-/g' | sed 's/,/-/g' | awk -F- '{for(i=2;i<=NF;i++){printf("%s,%s\n", $1, $i)}}' | sed 's/;/,/g'
 done
-
 rm -rf output-data
